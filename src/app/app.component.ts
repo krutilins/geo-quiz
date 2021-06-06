d9ade39d858c497419ef0e54743fd24f653ac23b
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-06-06 17:05:25 +0300

    set-up google-map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'geo-quiz';
+export class AppComponent implements OnInit {
+  public zoom = 12;
+
+  public center: google.maps.LatLngLiteral = { lat: 0, lng: 0 };
+
+  public options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  }
+
+  public ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+    })
+  }
+
+  public zoomIn() {
+    if (this.zoom < (this.options.maxZoom || 15)) this.zoom++
+  }
+
+  public zoomOut() {
+    if (this.zoom > (this.options.minZoom || 0)) this.zoom--
+  }
+
+  public click(event: google.maps.MouseEvent) {
+    console.log(event.latLng.toJSON())
+    console.log(event.latLng.toString())
+    console.log(event.latLng.toUrlValue())
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  battutaAPIKey: '9957eb7ad5699bf197440858abd1ddc3'
 };
 
 /*
