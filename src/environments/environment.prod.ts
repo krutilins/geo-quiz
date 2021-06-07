fa377e36bfbaa5b644f468a10a58cb016d5bf779
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2021-06-07 18:02:51 +0300

    update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  battutaAPIKey: '9957eb7ad5699bf197440858abd1ddc3',
+  geoDataSourceAPIKey: 'G9YC41KSQ3Y6FGR8TXCRTTDP1BVJMKA5'
 };
